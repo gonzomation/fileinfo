d0a6e79e0f83249fbfaf3c160993f08fe5d8ae1c
gonzomation <99704302+gonzomation@users.noreply.github.com>
2022-02-17 20:47:48 -0300

    Hashing directories; file listing; hashes to tsv

--- fileinfo.py
+++ fileinfo.py
@@ -4,10 +4,17 @@ import hashlib
 
 
 def main():
-    file = str(sys.argv[1])
+    path = str(sys.argv[1])
     show_header()
-    md5 = calc_hashes(file)
-    print(f"md5 of {file} is {md5}")
+    list_files(path)
+    with open(output, 'r', encoding='utf-8') as list:
+        for file in list:
+            md5 = calc_hashes(file.rstrip('\r\n'))
+            with open("hashed.tsv", 'a', encoding='utf-8') as fout:
+                filet = file.rstrip('\r\n')
+                data = f"{filet}\t{md5}"
+                fout.write(''.join(data))
+                fout.write("\n")
 
 
 def show_header():
@@ -16,9 +23,22 @@ def show_header():
     print("----------------------------------")
 
 
-def calc_hashes(path, block_size=64 * 128, human_readable=True):
+def list_files(path):
+    global output
+    output = os.path.join(os.getcwd(), 'list.txt')
+
+    for cwd, subdirs, files in os.walk(path):
+        for file in files:
+            with open(output, 'a', encoding='utf-8') as fout:
+                fout.write(os.path.join(cwd, file))
+                fout.write('\n')
+
+    return output
+
+
+def calc_hashes(file, block_size=64 * 128, human_readable=True):
     md5 = hashlib.md5()
-    with open(path, 'rb') as fin:
+    with open(file, 'rb') as fin:
         for chunk in iter(lambda: fin.read(block_size), b''):
             md5.update(chunk)
     if human_readable:
