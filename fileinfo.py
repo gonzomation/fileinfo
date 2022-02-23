e38f632565171eb9ed5ef9ff1066c83aad1bed48
gonzomation <99704302+gonzomation@users.noreply.github.com>
2022-02-23 00:19:36 -0300

    Multihashing including CRC32

--- fileinfo.py
+++ fileinfo.py
@@ -1,19 +1,38 @@
 import sys
 import os
 import hashlib
+import zlib
 
 
 def main():
     path = str(sys.argv[1])
+
+    hashes = {
+        'crc32': True,
+        'md5': True,
+        'sha1': True,
+        'sha256': False,
+        'sha512': False
+    }
+
+    chosen = []
+    for key, value in hashes.items():
+        if value:
+            chosen.append(key)
+
     show_header()
     list_files(path)
+
     with open(output, 'r', encoding='utf-8') as list:
+        crc = ''
         for file in list:
-            md5 = calc_hashes(file.rstrip('\r\n'))
+            if "crc32" in chosen:
+                crc = calc_crc32(file.rstrip('\r\n'))
+            hashes = '\t'.join(filter(None, calc_hashlib(file.rstrip('\r\n'), chosen)))
             with open("hashed.tsv", 'a', encoding='utf-8') as fout:
                 filet = file.rstrip('\r\n')
-                data = f"{filet}\t{md5}"
-                fout.write(''.join(data))
+                data = filet, crc, hashes
+                fout.write("\t".join(filter(None, data)))
                 fout.write("\n")
 
 
@@ -36,14 +55,27 @@ def list_files(path):
     return output
 
 
-def calc_hashes(file, block_size=64 * 128, human_readable=True):
-    md5 = hashlib.md5()
+def calc_crc32(file):
     with open(file, 'rb') as fin:
-        for chunk in iter(lambda: fin.read(block_size), b''):
-            md5.update(chunk)
-    if human_readable:
-        return md5.hexdigest()
-    return md5.digest()
+        crc32 = 0
+        while True:
+            s = fin.read(65536)
+            if not s:
+                break
+            crc32 = zlib.crc32(s, crc32)
+        return "%08X" % (crc32 & 0xFFFFFFFF)
+
+
+def calc_hashlib(file, chosen, block_size=64 * 128, human_readable=True):
+    results = []
+    for hash in chosen:
+        if hash != "crc32":
+            current = eval("hashlib."+hash+"()")
+            with open(file, 'rb') as fin:
+                for chunk in iter(lambda: fin.read(block_size), b''):
+                    current.update(chunk)
+                results.append(current.hexdigest())
+    return results
 
 
 if __name__ == '__main__':
